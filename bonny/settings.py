3911f3320677de6e5bdde4b4380f7c472a34f8fd
knowhere1998 <darekar.amey@gmail.com>
2019-02-28 16:28:02 +0530

    Removing unnesessary Corsheader Middleware

--- bonny/settings.py
+++ bonny/settings.py
@@ -63,8 +63,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-
 ]
 
 ROOT_URLCONF = 'bonny.urls'
